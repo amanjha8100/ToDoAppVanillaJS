91d62a980cb7a5b6647b5f649336c815b9b97059
Aman Prakash Jha <amanjha7151@gmail.com>
2021-05-09 01:27:32 +0530

    list items vanilla js

--- app.js
+++ app.js
@@ -3,5 +3,38 @@ const todoInput = document.querySelector('.todo-input');
 const todoButton = document.querySelector('.todo-button');
 const todoList = document.querySelector('.todo-list');
 //Event listeners
-
+todoButton.addEventListener('click',addTodo);
 //Functions
+function addTodo(event){
+    //prevent form from submitting
+    event.preventDefault();
+
+    //Todo DIV
+    const todoDiv = document.createElement("div");
+    todoDiv.classList.add("todo");
+
+    //create ListItem
+    const newTodo = document.createElement('li');
+    newTodo.innerText = 'hey';
+    newTodo.classList.add('todo-item');
+    //list item under div tag by appending child
+    todoDiv.appendChild(newTodo);
+
+    //Checkmark button
+    const completedButton = document.createElement('button');
+    completedButton.innerHTML = '<i class="fas fa-check"></i>';
+    completedButton.classList.add("complete-btn");
+    //added completed button to div tag
+    todoDiv.appendChild(completedButton);
+
+
+    //Trash button
+    const trashButton = document.createElement('button');
+    trashButton.innerHTML = '<i class="fas fa-trash"></i>';
+    trashButton.classList.add("complete-btn");
+    //added trash button to div tag
+    todoDiv.appendChild(trashButton);
+
+    //APPEND TO LIST
+    todoList.appendChild(todoDiv);
+}
