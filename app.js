84125e8cf090b3dd63ba790b0dbe33a41412f6b3
Aman Prakash Jha <amanjha7151@gmail.com>
2021-05-09 01:12:28 +0530

    code base

---
+++ app.js
@@ -0,0 +1,7 @@
+//Selectors
+const todoInput = document.querySelector('.todo-input');
+const todoButton = document.querySelector('.todo-button');
+const todoList = document.querySelector('.todo-list');
+//Event listeners
+
+//Functions
