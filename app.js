51abaf813c01e53cc1cbe8c67445a71866381b75
Aman Prakash Jha <amanjha7151@gmail.com>
2021-05-09 02:45:17 +0530

    added filters

--- app.js
+++ app.js
@@ -2,8 +2,11 @@
 const todoInput = document.querySelector('.todo-input');
 const todoButton = document.querySelector('.todo-button');
 const todoList = document.querySelector('.todo-list');
+const filterOption = document.querySelector(".filter-todo");
 //Event listeners
 todoButton.addEventListener('click',addTodo);
+todoList.addEventListener('click',deleteCheck);
+filterOption.addEventListener('click',filterTodo);
 //Functions
 function addTodo(event){
     //prevent form from submitting
@@ -40,4 +43,52 @@ function addTodo(event){
 
     //Clear Todo input value
     todoInput.value="";
+}
+
+function deleteCheck(e){
+    console.log(e.target);
+    const item = e.target;
+    //Delete ToDo
+    if(item.classList[0] === 'trash-btn'){
+        const todo = item.parentElement;
+        //Animation
+        todo.classList.add("fall");
+        //todo.remove();
+        todo.addEventListener('transitionend',function(){
+            todo.remove();
+        });
+    }
+
+    //Checkmark
+    if(item.classList[0]==='complete-btn'){
+        const todo = item.parentElement;
+        todo.classList.toggle("completed");
+    }
+}
+
+function filterTodo(e){
+    const todos = todoList.childNodes;
+    todos.forEach(function(todo){
+        switch(e.target.value){
+            case "all":
+                todo.style.display = "flex";
+                break;
+            case "completed":
+                if(todo.classList.contains('completed')){
+                    todo.style.display = 'flex';
+                }
+                else{
+                    todo.style.display = 'none';
+                }
+                break;
+            case "uncompleted":
+                if(!todo.classList.contains('completed')){
+                    todo.style.display = 'flex';
+                }
+                else{
+                    todo.style.display = 'none';
+                }
+                break;
+        }
+    });
 }
